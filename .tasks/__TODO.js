ce670171bfcf529b2b19707431b2376d137a4264
Brandtley <labs@gbox.name>
2016-03-10 17:08:24 -0600

    adding more TODOs to the pile

--- .tasks/__TODO.js
+++ .tasks/__TODO.js
@@ -1,1 +1,5 @@
-theme setting validator - check if theme setting is used in theme assets
+// TODO: theme setting validator - check if theme setting is used in theme assets
+// TODO: local less compilation with new @theme-settings-settingName syntax
+// TODO: bug docs team about missing info on @theme-settings-settingName
+// TODO: file bug with product that opening Site Builder > Editor in new tab from Settings page causes "Error retrieving theme settings configuration."
+// TODO: themeSettings in LESS do not respect string interpolation of @{theme-settings-settingName}
